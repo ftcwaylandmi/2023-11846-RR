f94c6f068b87edea8f23b9d26f487efbd7e176f7
Wayland FTC Robotics <ftc@waylandunion.org>
2023-12-07 17:40:54 -0500

    2023/12/7- Rolled Autons into Main
    2023/12/5 - TeleOpB now fully functional, all buttons, motors, servos moving in the right direction.  NEXT... need to implement routines for pickup and delivery.
    2023/11/30- Merged Spener's TeleOp into this base RR project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Subsystems/SensorSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Subsystems/SensorSubsystem.java
@@ -1,11 +1,51 @@
 package org.firstinspires.ftc.teamcode.drive.Subsystems;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import android.view.TouchDelegate;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 public class SensorSubsystem {
 
+    public RevColorSensorV3 colorSensor;
+
+    public TouchSensor touchSensor;
+
+    public DigitalChannel magnetSensor;
+
     public SensorSubsystem(HardwareMap hardwareMap){
+        colorSensor = hardwareMap.get(RevColorSensorV3.class,"colorSensor");
+        touchSensor = hardwareMap.get(TouchSensor.class,"armSensor");
+        magnetSensor = hardwareMap.get(DigitalChannel.class,"eleSensor");
+    }
+
+    public int ReturnColor() {
+        return colorSensor.argb();
 
     }
 
+ //   public int get() {
+ //       return colorSensor.red();
+ //   }
+
+    public int getColor(){
+        int r = colorSensor.red();
+        int g = colorSensor.green();
+        int b = colorSensor.blue();
+
+        if(r*2 >= g && r >= b){
+            // Red
+            return 2;
+        } else if(g*0.5 >= b && g*0.5 >= r){
+            //Green
+            return 1;
+        } else if(b*2 > r && b > g){
+            //blue
+            return 2;
+        }
+        return 1;
+    }
 }
